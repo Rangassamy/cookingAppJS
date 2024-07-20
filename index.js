e25542b69e5fe3bdd2b2e7dddcaf4102196dae0f
Rangassamy <enzorangassamy@gmail.com>
2024-07-20 11:04:27 -0400

    added the javascript first version, can't see instructions and ingredients

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
+const recipeName = document.getElementById("recipeName");
+const recipeCategorie = document.getElementById("recipeCategorie");
+const recipeInstrusctions = document.getElementById("recipeInstructions");
+const ingredients = document.querySelectorAll(".ingredients");
+const submitButton = document.getElementById("recipeSubmit");
+const recipesContainer = document.getElementById("recipesRender");
+const recipe = document.querySelectorAll(".recipe");
+
 class Recipe {
-  constructor(title, ingredients, instructions) {
-    this.title = title;
-    this.ingredients = ingredients;
+  constructor(name, categorie, instructions, ingredients) {
+    this.name = name;
+    this.categorie = categorie;
     this.instructions = instructions;
+    this.ingredients = ingredients;
   }
 }
 
-const recipes = [
-  new Recipe("Pato pesto", "pate", "cuir dans l'eau chaude"),
-  new Recipe("Pate carbo", "pates", "cuir dans l'eau"),
-  new Recipe("Pizza", "pizza", "faut faire cuir au four"),
-  new Recipe("Colombo", "colombo", "faut faire cuire"),
-];
+submitButton.addEventListener("click", () => {
+  const ingredientsArray = [];
+
+  ingredients.forEach((ingredient) => {
+    if (ingredient.value !== "") {
+      ingredientsArray.push(ingredient.value);
+    }
+  });
+
+  const recipe = new Recipe(
+    recipeName.value,
+    recipeCategorie.value,
+    recipeInstrusctions.value,
+    ingredientsArray
+  );
+
+  recipesContainer.innerHTML += ` 
+  <li class="recipe"">
+   <h2>${recipe.name}</h2>
+   <h3>${recipe.categorie}</h3>
+   <button id="ingredients">Ingredients</button>
+   <button id="Instructions">Intructions</button>
+  </li>
+  `;
+
+  recipeName.value = "";
+  recipeCategorie.value = "";
+  recipeInstrusctions.value = "";
+});
